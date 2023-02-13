4c381c46b3c92ac298ea5203c28d616b5ca63704
applemango <apple.mangoooo97@gmail.com>
2023-02-13 23:03:33 +0900

    fix design

--- pages/create/find.tsx
+++ pages/create/find.tsx
@@ -45,39 +45,59 @@ const Find = () => {
     },[router])
     return <div className={styles.container_create}>
         <FindComponent data={data} />
-        <button onClick={()=> {
-            if(!data)
-                return
-            setData((data: find | null)=> {
-                if(!data)
-                    return null
-                return {
-                    data: findGenerator(words, data?.x, data?.y),
-                    x: data.x,
-                    y: data.y
-                }
-            })
-        }}>generate</button>
-        <input type="text" value={exportData}  />
-        <button onClick={()=> {
-            try {
+        <div>
+            <button onClick={()=> {
                 if(!data)
                     return
-                const data_: findCreate = {
-                    x: data.x,
-                    y: data.y,
-                    data: data.data,
-                    words: words
-                }
-                const json = saveObject(data_)
-                setExportData(json)
-            } catch (e) {}
-        }}  >export</button>
-        <button onClick={()=> {
-            window.open(`/play/find?data=${exportData}`, "create");
-        }}>open new tab</button>
+                setData((data: find | null)=> {
+                    if(!data)
+                        return null
+                    return {
+                        data: findGenerator(words, data?.x, data?.y),
+                        x: data.x,
+                        y: data.y
+                    }
+                })
+            }} className={styles.button}>
+                <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-rotate-clockwise" width="42" height="42" viewBox="0 0 24 24" stroke-width="2" stroke="#0070f3" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                    <path d="M4.05 11a8 8 0 1 1 .5 4m-.5 5v-5h5" />
+                </svg>
+            </button>
+            <input className={styles.button_textarea} type="text" value={exportData}  />
+            <button onClick={()=> {
+                try {
+                    if(!data)
+                        return
+                    const data_: findCreate = {
+                        x: data.x,
+                        y: data.y,
+                        data: data.data,
+                        words: words
+                    }
+                    const json = saveObject(data_)
+                    setExportData(json)
+                } catch (e) {}
+            }} className={styles.button}><svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-writing" width="42" height="42" viewBox="0 0 24 24" stroke-width="2" stroke="#0070f3" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                    <path d="M20 17v-12c0 -1.121 -.879 -2 -2 -2s-2 .879 -2 2v12l2 2l2 -2z" />
+                    <path d="M16 7h4" />
+                    <path d="M18 19h-13a2 2 0 1 1 0 -4h4a2 2 0 1 0 0 -4h-3" />
+                </svg>
+            </button>
+            <button onClick={()=> {
+                window.open(`/play/find?data=${exportData}`, "create");
+            }} className={styles.button}>
+                <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-arrow-narrow-up" width="42" height="42" viewBox="0 0 24 24" stroke-width="2" stroke="#0070f3" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                    <line x1="12" y1="5" x2="12" y2="19" />
+                    <line x1="16" y1="9" x2="12" y2="5" />
+                    <line x1="8" y1="9" x2="12" y2="5" />
+                </svg>
+            </button>
+        </div>
         <div>
-            <input onChange={(e: any)=> {
+        <input onChange={(e: any)=> {
                 if(!data) return
                 setData((data: find | null)=> {
                     if(!data) return null
